8265e3bfe1f649250dac79c45e845516d9825cb6
wcrowe <crowe.will@gmail.com>
2015-11-18 10:15:02 -0500

    Check in.

---
+++ OvenStuff.sql
@@ -0,0 +1,11 @@
+drop table #t
+create table #t (R int identity(6,1), Prog varchar(2))
+
+insert #t(Prog) values('3')
+insert #t(Prog) values('4')
+insert #t(Prog) values('5')
+insert #t(Prog) values('6')
+insert #t(Prog) values('7')
+insert #t(Prog) values('8')
+insert #t(Prog) values('9')
+insert #t(Prog) values('10')
